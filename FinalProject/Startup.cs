be1afa4db69a365f0a57c452dd54dd8aee46b6fd
Anthony Perez <36647150+Perezanthony117@users.noreply.github.com>
2018-07-18 20:45:02 -0600

    Cleaned up the HTML and added Site.js changes

--- FinalProject/Startup.cs
+++ FinalProject/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebSocketManager;
 
 namespace FinalProject
 {
@@ -22,6 +23,7 @@ namespace FinalProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddWebSocketManager();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
--- FinalProject/wwwroot/js/site.js
+++ FinalProject/wwwroot/js/site.js
@@ -1,1 +1,96 @@
-﻿// Write your JavaScript code.
+﻿var connection;
+var user;
+var users = [];
+
+window.onload = function () {
+
+    connection = new WebSocketManager.Connection("ws://localhost:5000/server");
+
+    user = new user();
+
+    connection.connectionMethods.onConnected = () => {
+        user.id = connection.connectionId;
+        //("Name of Method to invoke on server",SocketID,Content to pass)
+        connection.invoke("ConnectedUser", connection.connectionId, JSON.stringify(user));
+
+    }
+
+    connection.connectionMethods.onDisconnected = () => {
+
+        connection.invoke("DisconnectedUser", connection.connectionId, "");
+
+    }
+
+    connection.clientMethods["pingUsers"] = (serUsers) => {
+        users = JSON.parse(serUser);
+        console.log(users);
+    };
+
+    connection.start()
+
+    $(window).unload(function () {
+        connection.invoke("DisconnectedUser", connection.connectionId, "");
+    });
+}
+
+
+function user() {
+    this.id = "";
+    this.color = "";
+}
+
+function block() {
+    this.x = "";
+    this.y = "";
+    this.alive = "";
+}
+
+
+
+function Start() {
+    startGame = true;
+    connection.invoke("startGame", connection.connectionId, JSON.stringify(startGame));
+}
+
+function Stop() {
+    startGame = false;
+    connection.invoke("stopGame", connection.connectionId, JSON.stringify(startGame));
+}
+
+window.onload = function () {
+
+    connection = new WebSocketManager.Connection("ws://localhost:5000/server");
+
+    user = new user();
+
+    connection.connectionMethods.onConnected = () => {
+        user.id = connection.connectionId;
+        //("Name of Method to invoke on server",SocketID,Content to pass)
+        connection.invoke("ConnectedUser", connection.connectionId, JSON.stringify(user));
+
+    }
+
+    connection.connectionMethods.onDisconnected = () => {
+
+        connection.invoke("DisconnectedUser", connection.connectionId, "");
+
+    }
+
+    connection.clientMethods["pingUsers"] = (serUsers) => {
+        users = JSON.parse(serUser);
+        console.log(users);
+    };
+
+    connection.clientMethods["ReceiveUpdateAsXYColor"] = (socketId, x, y, r, g, b) => {
+        // console.log(r + " " + g + " " + b);
+        var colorToUse = "#" + r + g + b;
+        // console.log(colorToUse + "(" + x + "," + y + ")");
+        assignColorToSquare(x, y, colorToUse);
+    };
+
+    connection.start()
+
+    $(window).unload(function () {
+        connection.invoke("DisconnectedUser", connection.connectionId, "");
+    });
+}
