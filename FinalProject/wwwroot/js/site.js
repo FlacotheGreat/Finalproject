382ae69a1c58ea19ec5ff64f54d17fe4fcd2ad60
dylan <dylanswaters@gmail.com>
2018-07-18 22:03:46 -0600

    created a few things we'll need as well as some js to support it

--- FinalProject/wwwroot/js/site.js
+++ FinalProject/wwwroot/js/site.js
@@ -1,1 +1,20 @@
-﻿// Write your JavaScript code.
+﻿//what's needed
+//button control for logging in or creating a new user
+function createNewUser(){
+
+}
+
+function login(){
+  console.log("login attempt made");
+  var username = document.getElementById("userName").value;
+  var password = document.getElementById("password").value;
+  //send this to the server
+  //if result is successful
+  window.location = "~/Views/Home/chart.cshtml";
+}
+
+//pass name of stock or company to add it as one of the users companies
+
+//getting data out to give to js charts
+
+//displaying purchased stocks on user page
