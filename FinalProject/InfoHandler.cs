a43bebac2c5248ea53b0b0a58d07f9cc148afd6c
Anthony <perezanthony117@gmail.com>
2018-07-18 19:14:37 -0600

    added files to work with websockets

---
+++ FinalProject/InfoHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSocketManager;
+
+namespace FinalProject
+{
+    public class InfoHandler : WebSocketHandler
+    {
+
+        public InfoHandler(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
+        {
+        }
+
+
+    }
+}
