001ce3782b707167dbf2180b25ea4c56e40a621c
dylan <dylanswaters@gmail.com>
2018-07-26 19:46:28 -0600

    worked on understanding how charts worked, changed a few things

--- FinalProject/wwwroot/js/site.js
+++ FinalProject/wwwroot/js/site.js
@@ -1,8 +1,27 @@
-
 //what's needed
 //getting data out to give to js charts
 
+//this needs to accept preferably a list of names and quantity
+function displayChart(companyAndPriceList, chartChoice) {
+  //get and clear the canvas
+  var c = document.getElementById("displayChart");
+  var ctx = c.getContext("2d");
+  // ctx.clearRect(0, 0, 600, 600);
+  //read list
+  //give it to the chart
+  if(chartChoice = "bar"){
+
+  }else if(chartChoice = "pie"){
+
+  }else if(chartChoice = "line"){
+
+  }
+}
+
 //displaying purchased stocks on user page
+function createStockTblFromJson(){
+
+}
 
 ﻿var connection;
 var user;
@@ -43,42 +62,4 @@ window.onload = function () {
 function user() {
     this.id = "";
     this.color = "";
-}
-
-window.onload = function () {
-
-    connection = new WebSocketManager.Connection("ws://localhost:5000/server");
-
-    user = new user();
-
-    connection.connectionMethods.onConnected = () => {
-        user.id = connection.connectionId;
-        //("Name of Method to invoke on server",SocketID,Content to pass)
-        connection.invoke("ConnectedUser", connection.connectionId, JSON.stringify(user));
-
-    }
-
-    connection.connectionMethods.onDisconnected = () => {
-
-        connection.invoke("DisconnectedUser", connection.connectionId, "");
-
-    }
-
-    connection.clientMethods["pingUsers"] = (serUsers) => {
-        users = JSON.parse(serUser);
-        console.log(users);
-    };
-
-    connection.clientMethods["ReceiveUpdateAsXYColor"] = (socketId, x, y, r, g, b) => {
-        // console.log(r + " " + g + " " + b);
-        var colorToUse = "#" + r + g + b;
-        // console.log(colorToUse + "(" + x + "," + y + ")");
-        assignColorToSquare(x, y, colorToUse);
-    };
-
-    connection.start()
-
-    $(window).unload(function () {
-        connection.invoke("DisconnectedUser", connection.connectionId, "");
-    });
 }
