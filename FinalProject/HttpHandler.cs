ed300746b15d3f84557c6fab747525f373abdd09
David Kirschman <kadivadavidak@gmail.com>
2018-07-31 21:34:17 -0600

    Setup API connection

---
+++ FinalProject/HttpHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+
+namespace FinalProject
+{
+    public class HttpHandler
+    {
+        static async void GetData(string uri)
+        {
+            // From this vid https://www.youtube.com/watch?v=XAHF8TzFmJI
+            using (HttpClient client = new HttpClient())
+            using (HttpResponseMessage response = await client.GetAsync(uri))
+            using (HttpContent content = response.Content)
+            {
+                string data = await content.ReadAsStringAsync();
+
+                if (data != null)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
--- FinalProject/InfoHandler.cs
+++ FinalProject/InfoHandler.cs
@@ -1,36 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using WebSocketManager;
 
 namespace FinalProject
 {
     public class InfoHandler : WebSocketHandler
     {
-
         public InfoHandler(WebSocketConnectionManager webSocketConnectionManager) : base(webSocketConnectionManager)
         {
         }
 
         //basically this one should give what's needed for the table
         public async Task giveJSONData(string socketId){
-          //obtain user json data
-          // jsonComp1 =
-          // jsonComp2 =
-          // jsonComp3 =
-          //pass that data back to client
-          //await InvokeClientMethodToAllAsync("ReceiveUserChoicesJson", jsonComp1, jsonComp2, jsonComp3);
+            //obtain user json data
+            // jsonComp1 =
+            // jsonComp2 =
+            // jsonComp3 =
+            //pass that data back to client
+            //await InvokeClientMethodToAllAsync("ReceiveUserChoicesJson", jsonComp1, jsonComp2, jsonComp3);
         }
 
         //this function should give what's needed for a chart
         public async Task giveChartData(string socketId){
-          //obtain user json data
-          // jsonComp1 =
-          // jsonComp2 =
-          // jsonComp3 =
-          //pass that data back to client
-          //await InvokeClientMethodToAllAsync("ReceiveJSONChartData", jsonComp1, jsonComp2, jsonComp3);
+            //obtain user json data
+            // jsonComp1 =
+            // jsonComp2 =
+            // jsonComp3 =
+            //pass that data back to client
+            //await InvokeClientMethodToAllAsync("ReceiveJSONChartData", jsonComp1, jsonComp2, jsonComp3);
         }
     }
 }
