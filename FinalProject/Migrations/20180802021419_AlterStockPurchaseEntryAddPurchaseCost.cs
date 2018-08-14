7f057ed6f470ac897824bca4a160b7a497319631
David Kirschman <kadivadavidak@gmail.com>
2018-08-13 20:18:07 -0600

    New migration changes.

---
+++ FinalProject/Migrations/20180802021419_AlterStockPurchaseEntryAddPurchaseCost.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace FinalProject.Migrations
+{
+    public partial class AlterStockPurchaseEntryAddPurchaseCost : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "purchase_cost",
+                table: "stock_purchase_entry",
+                nullable: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
